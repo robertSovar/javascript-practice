8cf09906140b1d9fc5f15d66c21d616ec8485abe
robertSovar <133514926+robertSovar@users.noreply.github.com>
2024-08-19 22:30:03 +0300

    practice

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
-const text = "Hello, World!";
+console.dir(document);
 
-console.log(text);
+const btn = document.getElementById("createBtn");
+console.dir(btn);
+btn.addEventListener("click", () => {
+  const div = document.createElement("div");
+  div.classList.add("personal-div");
+  div.innerHTML = "This is a div";
+  document.body.appendChild(div);
+});
+
+const removeBtn = document.getElementById("removeDiv");
+
+removeBtn.addEventListener("click", () => {
+  const div = document.querySelector(".personal-div");
+
+  if (div === null) {
+    const alert = document.createElement("p");
+    alert.innerHTML = "No div found";
+    document.body.appendChild(alert);
+  } else {
+    document.body.removeChild(div);
+  }
+});
